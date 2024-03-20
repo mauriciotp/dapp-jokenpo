d00b2d23756c3123b09492a33f6dfde223a1f180
Mauricio <mauricio.tp_@outlook.com>
2024-03-19 22:20:36 -0300

    feat: create JKPLibrary

--- blockchain/contracts/IJoKenPo.sol
+++ blockchain/contracts/IJoKenPo.sol
@@ -2,19 +2,9 @@
 
 pragma solidity ^0.8.24;
 
+import "./JKPLibrary.sol";
+
 interface IJoKenPo {
-    enum Options {
-        NONE,
-        ROCK,
-        PAPER,
-        SCISSORS
-    } //0, 1, 2, 3
-
-    struct Player {
-        address wallet;
-        uint32 wins;
-    }
-
     function getResult() external view returns (string memory);
 
     function getBid() external view returns (uint256);
@@ -27,7 +17,10 @@ interface IJoKenPo {
 
     function getBalance() external view returns (uint);
 
-    function play(Options newChoice) external payable;
+    function play(JKPLibrary.Options newChoice) external payable;
 
-    function getLeaderboard() external view returns (Player[] memory);
+    function getLeaderboard()
+        external
+        view
+        returns (JKPLibrary.Player[] memory);
 }
--- blockchain/contracts/JKPAdapter.sol
+++ blockchain/contracts/JKPAdapter.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.24;
 
 import "./IJoKenPo.sol";
+import "./JKPLibrary.sol";
 
 contract JKPAdapter {
     IJoKenPo private joKenPo;
@@ -10,6 +11,21 @@ contract JKPAdapter {
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function getAddress() external view returns (address) {
+        return address(joKenPo);
+    }
+
+    function getResult() external view upgraded returns (string memory) {
+        return joKenPo.getResult();
+    }
+
+    function play(JKPLibrary.Options newChoice) external payable upgraded {}
+
+    modifier upgraded() {
+        require(address(joKenPo) != address(0), "You must upgrade first");
+        _;
     }
 
     function upgrade(address newImplementation) external {
